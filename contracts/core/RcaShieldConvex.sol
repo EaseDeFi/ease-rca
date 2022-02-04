1b9d235f6c4763032b7ca3d682d9c60fc766d751
leekt <leekt216@gmail.com>
2022-02-04 15:29:38 +0900

    assuming that _token can be used in _shield

--- contracts/core/RcaShieldConvex.sol
+++ contracts/core/RcaShieldConvex.sol
@@ -33,13 +33,17 @@ contract RcaShieldConvex is RcaShieldBase {
 
     function purchase(
         address _token,
-        uint256 _amount,
-        uint256 _ethPrice,
-        bytes32[] calldata _priceProof
-    ) payable external {
-        require(msg.value > _amount * 1e18 / _ethPrice, "low");
-        controller.verifyPrice(address(this), _ethPrice, _priceProof);
-        IERC20(_token).transfer(msg.sender, _amount);
+        uint256 _amount, // token amount to buy
+        uint256 _tokenPrice,
+        bytes32[] calldata _tokenPriceProof,
+        uint256 _underlyingPrice,
+        bytes32[] calldata _underlyinPriceProof
+    ) external {
+        controller.verifyPrice(_token,  _tokenPrice, _tokenPriceProof);
+        controller.verifyPrice(address(this), _underlyingPrice, _underlyinPriceProof);
+        uint256 underlyingAmount = _amount * _tokenPrice / _underlyingPrice;
+        IERC20(_token).safeTransfer(msg.sender, _amount);
+        uToken.safeTransferFrom(msg.sender,address(this), underlyingAmount);
     }
 
     function _uBalance() internal view override returns(uint256) {
