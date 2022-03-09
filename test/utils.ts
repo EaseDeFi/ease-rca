16a3f8420647086c1e8260c67be21982cfbd1ad0
chiranz <chiranpoudyal@gmail.com>
2022-03-09 11:54:07 +0530

    test: rcaController independent full coverage tests

--- test/utils.ts
+++ test/utils.ts
@@ -1,5 +1,7 @@
 import { ethers } from "hardhat";
 import { providers, Contract, Signer, BigNumber } from "ethers";
+import { RcaController } from "../src/types/RcaController";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 export function hexSized(str: string, length: number): string {
   const raw = Buffer.from(str).toString("hex");
@@ -36,4 +38,42 @@ export async function mine() {
   const signers = await ethers.getSigners();
   const signer = signers[0];
   await (signer.provider as providers.JsonRpcProvider).send("evm_mine", []);
+}
+export function formatEther(amount: BigNumber): string {
+  return ethers.utils.formatEther(amount);
+}
+
+type SignatureArgs = {
+  controller: RcaController;
+  capOracle: SignerWithAddress;
+  userAddress: string;
+  amount: BigNumber;
+  shieldAddress: string;
+};
+type SignedDetails = {
+  expiry: BigNumber;
+  vInt: BigNumber;
+  r: string;
+  s: string;
+};
+
+export async function getSignatureDetailsFromCapOracle({
+  amount,
+  capOracle,
+  controller,
+  userAddress,
+  shieldAddress,
+}: SignatureArgs): Promise<SignedDetails> {
+  const nonce = await controller.nonces(userAddress);
+  const timestamp = await getTimestamp();
+  const expiry = timestamp.add(300);
+  const hash = await controller.getMessageHash(userAddress, shieldAddress, amount, nonce, expiry);
+  const signature = await capOracle.signMessage(ethers.utils.arrayify(hash));
+
+  const v = signature.substring(130, signature.length);
+  const r = "0x" + signature.substring(2, 66);
+  const s = "0x" + signature.substring(66, 130);
+  const vInt = BigNumber.from(parseInt(v, 16));
+
+  return { vInt, r, s, expiry };
 }
