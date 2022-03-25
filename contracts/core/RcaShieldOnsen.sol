fa1af9eb77faea941b167363199fa5dee90f5185
chiranz <chiranpoudyal@gmail.com>
2022-03-25 06:19:40 +0530

    feat: remove args and test update for sushi/onsen adapter

--- contracts/core/RcaShieldOnsen.sol
+++ contracts/core/RcaShieldOnsen.sol
@@ -10,8 +10,6 @@ contract RcaShieldOnsen is RcaShieldBase {
 
     IMasterChefV2 public immutable masterChef;
 
-    IERC20 public immutable sushi;
-
     uint256 public immutable pid;
 
     constructor(
@@ -22,11 +20,9 @@ contract RcaShieldOnsen is RcaShieldBase {
         address _governance,
         address _controller,
         IMasterChefV2 _masterChef,
-        IERC20 _sushi,
         uint256 _pid
     ) RcaShieldBase(_name, _symbol, _uToken, _uTokenDecimals, _governance, _controller) {
         masterChef = _masterChef;
-        sushi = _sushi;
         pid = _pid;
     }
 
