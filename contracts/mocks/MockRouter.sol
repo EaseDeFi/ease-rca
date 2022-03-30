50da0335ac5dfa42958aa86066c15479fcc1b11b
RobertMCForster <rforster510@yahoo.com>
2022-03-30 11:41:16 -0700

    Router tests

---
+++ contracts/mocks/MockRouter.sol
@@ -0,0 +1,9 @@
+/// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract MockRouter {
+    function routeTo(address _user, uint256 _amount, bytes calldata _data) external {
+        revert();
+    }
+}
