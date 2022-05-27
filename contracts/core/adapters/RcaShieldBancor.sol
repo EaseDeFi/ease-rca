0c3d165aa43e62ec1d41d6e2a8c1e34d41f8513d
dominikpre <dominik.pre@gmail.com>
2022-05-27 22:12:29 +0200

    Fix RcaShieldBancor to use StandardRewards contract

--- contracts/core/adapters/RcaShieldBancor.sol
+++ contracts/core/adapters/RcaShieldBancor.sol
@@ -9,6 +9,9 @@ contract RcaShieldBancor is RcaShieldNormalized {
     using SafeERC20 for IERC20Metadata;
 
     IBancorNetwork public immutable bancorNetwork;
+    IStandardRewards public immutable standardRewards;
+
+    uint256 public immutable id;
 
     constructor(
         string memory _name,
@@ -17,17 +20,22 @@ contract RcaShieldBancor is RcaShieldNormalized {
         uint256 _uTokenDecimals,
         address _governance,
         address _controller,
-        IBancorNetwork _bankorNetwork
+        IBancorNetwork _bankorNetwork,
+        IStandardRewards _standardRewards,
+        uint256 _id
     ) RcaShieldNormalized(_name, _symbol, _uToken, _uTokenDecimals, _governance, _controller) {
         bancorNetwork = _bankorNetwork;
+        standardRewards = _standardRewards;
+        id = _id;
 
         //TODO: maybe use depositPermitted instead of approve + deposit
         //TODO: Does the approve have to be safeApprove?
         //TODO: change masterchef to bancor contract
-        uToken.safeApprove(address(bancorNetwork), type(uint256).max);
+        // uToken.safeApprove(address(bancorNetwork), type(uint256).max);
     }
 
     function getReward() external {
+        //TODO:
     }
 
     function purchase(
@@ -53,7 +61,7 @@ contract RcaShieldBancor is RcaShieldNormalized {
         token.safeTransfer(msg.sender, _amount);
         uToken.safeTransferFrom(msg.sender, address(this), _normalizedUAmount(underlyingAmount));
 
-
+        standardRewards.depositAndJoin(id, underlyingAmount);
         // bancorNetwork.deposit(SafeERC20(uToken), underlyingAmount);
     }
 
