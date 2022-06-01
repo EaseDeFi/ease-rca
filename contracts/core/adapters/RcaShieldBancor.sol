e8af04e55b2eff9015311e092e4624d528916d9c
dominikpre <dominik.pre@gmail.com>
2022-06-01 13:51:45 +0200

    Implement _afterMint and getRewards functions. Fix purchase function

--- contracts/core/adapters/RcaShieldBancor.sol
+++ contracts/core/adapters/RcaShieldBancor.sol
@@ -4,11 +4,12 @@ pragma solidity ^0.8.11;
 
 import "../RcaShieldNormalized.sol";
 import "../../external/Bancor.sol";
+import "hardhat/console.sol";
 
 contract RcaShieldBancor is RcaShieldNormalized {
     using SafeERC20 for IERC20Metadata;
 
-    IBancorNetwork public immutable bancorNetwork;
+    // IBancorNetwork public immutable bancorNetwork;
     IStandardRewards public immutable standardRewards;
 
     uint256 public immutable id;
@@ -20,22 +21,18 @@ contract RcaShieldBancor is RcaShieldNormalized {
         uint256 _uTokenDecimals,
         address _governance,
         address _controller,
-        IBancorNetwork _bankorNetwork,
         IStandardRewards _standardRewards,
         uint256 _id
     ) RcaShieldNormalized(_name, _symbol, _uToken, _uTokenDecimals, _governance, _controller) {
-        bancorNetwork = _bankorNetwork;
         standardRewards = _standardRewards;
         id = _id;
-
-        //TODO: maybe use depositPermitted instead of approve + deposit
-        //TODO: Does the approve have to be safeApprove?
-        //TODO: change masterchef to bancor contract
-        // uToken.safeApprove(address(bancorNetwork), type(uint256).max);
+        uToken.safeApprove(address(standardRewards), type(uint256).max);
     }
 
     function getReward() external {
-        //TODO:
+        uint256[] memory t = new uint256[](1);
+        t[0] = id;
+        standardRewards.claimRewards(t);  
     }
 
     function purchase(
@@ -50,7 +47,7 @@ contract RcaShieldBancor is RcaShieldNormalized {
         controller.verifyPrice(_token, _tokenPrice, _tokenPriceProof);
         controller.verifyPrice(address(uToken), _underlyingPrice, _underlyinPriceProof);
         uint256 underlyingAmount = (_amount * _tokenPrice) / _underlyingPrice;
-        if (discount > 0) {
+        if(discount > 0) {
             underlyingAmount -= (underlyingAmount * discount) / DENOMINATOR;
         }
 
@@ -61,8 +58,7 @@ contract RcaShieldBancor is RcaShieldNormalized {
         token.safeTransfer(msg.sender, _amount);
         uToken.safeTransferFrom(msg.sender, address(this), _normalizedUAmount(underlyingAmount));
 
-        standardRewards.depositAndJoin(id, underlyingAmount);
-        // bancorNetwork.deposit(SafeERC20(uToken), underlyingAmount);
+        standardRewards.join(id, underlyingAmount);
     }
 
     function _uBalance() internal view override returns (uint256) {
@@ -70,7 +66,9 @@ contract RcaShieldBancor is RcaShieldNormalized {
     }
 
     function _afterMint(uint256 _uAmount) internal override {
-
+        // TODO: sometimes joining might not be worth it. Compounding rewards 
+        // occure anyways. Joining is only for additional BNT incentive rewards.  
+        standardRewards.join(id, _uAmount); 
     }
 
     function _afterRedeem(uint256 _uAmount) internal override {
